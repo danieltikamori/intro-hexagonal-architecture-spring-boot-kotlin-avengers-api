954f833d88f05f2243b0128af4de2459348ba892
Daniel Tikamori <tikamori.itiro@gmail.com>
2024-03-26 05:53:08 +0900

    Update to be nullable

--- src/main/kotlin/cc/tkmr/avengers/avengersapi/application/web/resource/response/AvengerResponse.kt
+++ src/main/kotlin/cc/tkmr/avengers/avengersapi/application/web/resource/response/AvengerResponse.kt
@@ -1,8 +1,21 @@
 package cc.tkmr.avengers.avengersapi.application.web.resource.response
 
+import cc.tkmr.avengers.avengersapi.domain.avenger.Avenger
+
 data class AvengerResponse(
     val nick: String,
     val person: String,
-    val description: String,
-    val history: String
+    val description: String?, // ? means nullable
+    val history: String?
 )
+// Static factory method
+{
+    companion object {
+        fun from(avenger: Avenger) = AvengerResponse(
+            nick = avenger.nick,
+            person = avenger.person,
+            description = avenger.description,
+            history = avenger.history
+        )
+    }
+}
--- src/main/kotlin/cc/tkmr/avengers/avengersapi/domain/avenger/Avenger.kt
+++ src/main/kotlin/cc/tkmr/avengers/avengersapi/domain/avenger/Avenger.kt
@@ -3,6 +3,6 @@ package cc.tkmr.avengers.avengersapi.domain.avenger
 data class Avenger(
     val nick: String,
     val person: String,
-    val description: String,
-    val history: String
+    val description: String?, // ? means nullable
+    val history: String?
 )
