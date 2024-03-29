89d76da95b006c5c6c47706ebe514a0b67bdf268
Daniel Tikamori <tikamori.itiro@gmail.com>
2024-03-29 22:02:25 +0900

    Update run arguments

--- start_api.sh
+++ start_api.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./mvnw spring-boot:run -Dspring-boot.run.profiles=dev -Dspring-boot.run.jvmArguments="-Xmx256m -Xms128m" -Dspring-boot.run.arguments="'--DB_USER=tkmr.avenger' '--DB_PASSWORD=tkmr.avenger' '--DB_NAME=avengers'"
+./mvnw spring-boot:run -Dspring-boot.run.profiles=dev -Dspring-boot.run.jvmArguments="-Xmx256m -Xms128m" -Dspring-boot.run.arguments="'--DB_USER=postgres' '--DB_PASSWORD=123456' '--DB_NAME=avengers_db'"
