b8d38df3d6abea9752c98dd408b8098d72e312e4
Daniel Tikamori <tikamori.itiro@gmail.com>
2024-03-30 09:54:05 +0900

    Add Dotenv module that loads environment variable from .env

--- src/main/kotlin/cc/tkmr/avengers/api/AvengersApiApplication.kt
+++ src/main/kotlin/cc/tkmr/avengers/api/AvengersApiApplication.kt
@@ -1,5 +1,6 @@
 package cc.tkmr.avengers.api
 
+import io.github.cdimascio.dotenv.Dotenv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +8,6 @@ import org.springframework.boot.runApplication
 class AvengersApiApplication
 
 fun main(args: Array<String>) {
+	Dotenv.configure().load()
 	runApplication<AvengersApiApplication>(*args)
 }
