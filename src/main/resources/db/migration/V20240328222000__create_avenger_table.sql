35150bfdb3370d0966fdd5dbba6bafa2c0473886
Daniel Tikamori <tikamori.itiro@gmail.com>
2024-03-30 09:58:35 +0900

    Fix: code indentation

--- src/main/resources/db/migration/V20240328222000__create_avenger_table.sql
+++ src/main/resources/db/migration/V20240328222000__create_avenger_table.sql
@@ -4,7 +4,7 @@ create table avenger (
                          person varchar(128),
                          description varchar(128),
                          history text,
-                             primary key (id)
+                         primary key (id)
 );
 
 alter table avenger add constraint UK_5r88eemotwgru6k0ilqb2ledh unique (nick);
